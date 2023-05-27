e6c251940eaeba339d5f6508240c63c80d492e81
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-27 16:49:27 +0300

    initial create

---
+++ lib/screens/past_workouts/past_workouts_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PastWorkoutsScreen extends StatefulWidget {
+  const PastWorkoutsScreen({super.key});
+
+  @override
+  State<PastWorkoutsScreen> createState() => _PastWorkoutsScreenState();
+}
+
+class _PastWorkoutsScreenState extends State<PastWorkoutsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
