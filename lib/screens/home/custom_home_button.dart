07753f166ea63b5871a16f5f7ebfce611dd4e453
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-28 15:41:54 +0300

    So much more shwit

--- lib/screens/home/custom_home_button.dart
+++ lib/screens/home/custom_home_button.dart
@@ -1,7 +1,16 @@
+import 'package:fitnesh/utils/navigation_utils.dart';
+import 'package:fitnesh/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class CustomHomeButton extends StatefulWidget {
-  const CustomHomeButton({super.key});
+  final Widget screen;
+  final String label;
+  final IconData icon;
+  const CustomHomeButton(
+      {super.key,
+      required this.screen,
+      required this.label,
+      required this.icon});
 
   @override
   State<CustomHomeButton> createState() => _CustomHomeButtonState();
@@ -11,8 +20,22 @@ class _CustomHomeButtonState extends State<CustomHomeButton> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
-      child: const Text("HomeButton"),
-    );
+        style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.zero)),
+        onPressed: () {
+          navigate(context, widget.screen);
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(widget.icon, size: 50),
+            const SizedBox(
+              height: 6.00,
+            ),
+            CustomText(
+              widget.label,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ));
   }
 }
